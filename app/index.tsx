50a35682024a2b8b9f414f1e2756884f8f8411f2
Shreeyanshu kesherwani <shreeyanshukesherwani@gmail.com>
2024-11-09 01:38:34 +0530

    changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Button, View, StyleSheet, SafeAreaView } from "react-native";
 
-export default function Index() {
+import { Amplify } from "aws-amplify";
+import TodoList from "../components/TodoList"; // Adjust the path as necessary
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+const SignOutButton = () => {
+  const { signOut } = useAuthenticator();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.signOutButton}>
+      <Button title="Sign Out" onPress={signOut} />
     </View>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Authenticator.Provider>
+      <Authenticator>
+        <SafeAreaView style={styles.container}>
+          <SignOutButton />
+          <TodoList />
+        </SafeAreaView>
+      </Authenticator>
+    </Authenticator.Provider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+  },
+  signOutButton: {
+    alignSelf: "flex-end",
+  },
+});
+
+export default App;
