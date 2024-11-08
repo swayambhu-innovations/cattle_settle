3ea08ed6a26ea135896d7b9b284b0badd554c9ce
Shreeyanshu kesherwani <shreeyanshukesherwani@gmail.com>
2024-11-09 01:53:34 +0530

    blank project

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,9 +10,8 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-      isDone: a.boolean()
     })
-    .authorization(allow => [allow.owner()])
+    .authorization((allow) => [allow.guest()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -20,8 +19,8 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'userPool'
-  }
+    defaultAuthorizationMode: 'iam',
+  },
 });
 
 /*== STEP 2 ===============================================================
--- app/index.tsx
+++ app/index.tsx
@@ -1,45 +1,15 @@
-import React from "react";
-import { Button, View, StyleSheet, SafeAreaView } from "react-native";
+import { Text, View } from "react-native";
 
-import { Amplify } from "aws-amplify";
-import TodoList from "../components/TodoList"; // Adjust the path as necessary
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
-
-import outputs from "../amplify_outputs.json";
-
-Amplify.configure(outputs);
-
-const SignOutButton = () => {
-  const { signOut } = useAuthenticator();
-
+export default function Index() {
   return (
-    <View style={styles.signOutButton}>
-      <Button title="Sign Out" onPress={signOut} />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
-};
-
-const App = () => {
-  return (
-    <Authenticator.Provider>
-      <Authenticator>
-        <SafeAreaView style={styles.container}>
-          <SignOutButton />
-          <TodoList />
-        </SafeAreaView>
-      </Authenticator>
-    </Authenticator.Provider>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 8,
-  },
-  signOutButton: {
-    alignSelf: "flex-end",
-  },
-});
-
-export default App;
+}
